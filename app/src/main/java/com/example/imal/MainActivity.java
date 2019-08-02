b0951df1cd107f89415bbd3257c810154fcf1b94
imalK96 <imalkrulez@gmail.com>
2019-08-02 20:57:05 +0530

    Added user profile, navigation from Dashboard to profile

--- app/src/main/java/com/example/imal/MainActivity.java
+++ app/src/main/java/com/example/imal/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.imal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    //private CardView profileCard;
+    private LinearLayout l1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        l1 = findViewById(R.id.profile);
+
+    }
+
+    public void profileNav(View v){
+        Intent profileIntent = new Intent(getBaseContext(), userProfile.class);
+        startActivity(profileIntent);
     }
 }
