4d18569064b310faf64912303407364c2b4b45d5
imalK96 <imalkrulez@gmail.com>
2019-08-05 19:28:54 +0530

    added register page

--- app/src/main/java/com/example/imal/LoginActivity.java
+++ app/src/main/java/com/example/imal/LoginActivity.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 public class LoginActivity extends AppCompatActivity {
 
     private Button loginBtn;
-    private TextView emailTxt, pwTxt;
+    private TextView emailTxt, pwTxt, register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +19,17 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         loginBtn = findViewById(R.id.loginBtn);
+        register = findViewById(R.id.createAcc);
 
     }
 
     public void Login(View v){
         Intent loginIntent = new Intent(getBaseContext(), MainActivity.class);
         startActivity(loginIntent);
+    }
+
+    public void registerRedirect(View v){
+        Intent regIntent = new Intent(getBaseContext(), registerUser.class);
+        startActivity(regIntent);
     }
 }
