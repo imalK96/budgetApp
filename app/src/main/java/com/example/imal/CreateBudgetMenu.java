244555c4853a73b1e9ae620bbbb7c770632eb991
nweerakeshara <thnuwanga@gmail.com>
2019-09-14 17:34:54 +0530

    BudgetMenu Added with 4 buttons

--- app/src/main/java/com/example/imal/CreateBudgetMenu.java
+++ app/src/main/java/com/example/imal/CreateBudgetMenu.java
@@ -2,7 +2,9 @@ package com.example.imal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateBudgetMenu extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class CreateBudgetMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_budget_menu);
+    }
+
+    public void enterBudget(View v){
+        Intent enterBudget = new Intent(getBaseContext(),CreateBudget.class);
+        startActivity(enterBudget);
     }
 }
