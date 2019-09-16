1c8c9d1fd63c01c3b882d5eece7361fad21ca795
nweerakeshara <thnuwanga@gmail.com>
2019-09-16 16:24:41 +0530

    Merge branch 'firebase' of https://github.com/imalK96/budgetApp
    # Conflicts:
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/imal/userProfile.java
+++ app/src/main/java/com/example/imal/userProfile.java
@@ -45,7 +45,7 @@ public class userProfile extends AppCompatActivity {
 
         userEmailTxt = findViewById(R.id.userPwOld);
         userPwTxt = findViewById(R.id.userPasswordNew);
-        displayNameTxt = findViewById(R.id.displayname);
+        displayNameTxt = findViewById(R.id.displayName);
 
         updateBtn = findViewById(R.id.updateCredBtn);
         DisableBtn = findViewById(R.id.DeleteBtn);
